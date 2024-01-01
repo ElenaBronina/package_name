3a0b9bfa52443249ece83c730662f1ebd0fe107f
Elena Bronina <aplicaziya@yandex.ru>
2024-01-01 16:29:00 +0300

    add test_capitalize.py

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from package_name.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
+    
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+    
 
 print('Все тесты пройдены!')
