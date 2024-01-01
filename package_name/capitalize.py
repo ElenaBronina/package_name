bb1181dfa12c3ccf819bf6213c10ccee78cdf652
Elena Bronina <aplicaziya@yandex.ru>
2024-01-01 15:12:36 +0300

    add package_name

---
+++ package_name/capitalize.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def capitalize(text):
+    if text == '':
+        return ''
+    first_char = text[0].upper()
+    rest_substring = text[1:]
+    return f'{first_char}{rest_substring}'
---
+++ tests/test_capitalize.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from package_name.capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
